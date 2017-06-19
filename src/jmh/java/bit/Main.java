70ec9ea15f66306ec1da077749d97e118f7acd74
Keiji Yoshida <keijiyoshida.mail@gmail.com>
2017-06-19 17:55:02 +0900

    Add files

--- src/jmh/java/bit/Main.java
+++ src/jmh/java/bit/Main.java
@@ -1,7 +1,8 @@
-package simple;
+package bit;
 
 import org.openjdk.jmh.annotations.*;
 
+import java.util.BitSet;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
@@ -9,13 +10,13 @@ import java.util.concurrent.TimeUnit;
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
 @BenchmarkMode(Mode.AverageTime)
 @Fork(value = 1, jvmArgsAppend = {
-        "-XX:-UseSuperWord",
+        "-XX:+UseSuperWord",
         "-XX:+UnlockDiagnosticVMOptions",
-        "-XX:CompileCommand=print,*Main.increment"})
+        "-XX:CompileCommand=print,*Main.convert"})
 @Warmup(iterations = 5)
 @Measurement(iterations = 10)
 public class Main {
-    public static final int SIZE = 1024;
+    public static final int SIZE = 10240;
 
     @State(Scope.Thread)
     public static class Context {
@@ -26,15 +27,15 @@ public class Main {
         public void setup() {
             Random random = new Random();
             for (int i = 0; i < SIZE; i++) {
-                values[i] = (byte)random.nextInt(Byte.MAX_VALUE - 1);
+                values[i] = (byte)random.nextInt(Byte.MAX_VALUE);
             }
         }
     }
 
     @Benchmark
-    public byte[] increment(Context context) {
+    public byte[] convert(Context context) {
         for (int i = 0; i < SIZE; i++) {
-            context.results[i] = (byte)(context.values[i] + 1);
+            context.results[i] = (byte)(context.values[i] ^ 0x22);
         }
         return context.results;
     }
